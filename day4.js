0c92cd1cfa28c1513154ec09f94f1a24093aba03
unknown <aradhyas1973@gmail.com>
2025-09-17 09:59:32 +0530

    Flex and grid

---
+++ day4.js
@@ -0,0 +1,25 @@
+// output method
+console.log("FSD Class");
+// input method
+// let a = prompt("Enter a number:");
+// console.log(a);
+
+// diffrence bewtween var and char
+// var is redeclare
+var a = 20;
+ var a = 30;   
+
+
+ let b = 30;
+ console.log("enter the first value:" +b);
+ b  = 70;
+ console.log("After the regestraion of b:"+b);
+ {
+    let name = "Archana";
+    console.log("name:"+name);
+    name = "XYZ";
+    console.log("name:"+name);
+    b = 60;
+ }
+//  string
+let firstName = "Aradhya";
